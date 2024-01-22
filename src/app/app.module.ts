cf09268611e1fab15b5e836230d8bf470224d10e
Sachin Yadav <s2244yadav@gmail.com>
2024-01-23 00:57:14 +0530

    added  more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ContactComponent } from './contact/contact.component';
     NavbarComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,AppRoutingModule
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  facebookurl!: string;
+  linkedinurl!: string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  shareFacebookProfile() {
+    var facebookProfileUrl = 'https://www.facebook.com/998428336sachin@gmail.com';
+    // Create the Facebook share URL
+    this.facebookurl = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(facebookProfileUrl);
 
+    // Open the share dialog in a new window or tab
+    window.open(this.facebookurl, '_blank');
+  }
+
+  shareLinkedInProfile() {
+    this.linkedinurl = `https://www.linkedin.com/in/sachin-yadav-81966b229?utm_
+    source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app `;
+    // Open the share dialog in a new window or tab
+    window.open(this.linkedinurl, '_blank');
+  }
 }
